b5a30835d5a89a55f5428f5831ca071fd4fed1eb
chenquzhao <chec98qu@gmail.com>
2020-10-06 15:21:49 +0800

    Build basic web framework & Connect Flask2React

---
+++ filmfinder/src/App.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react';
+
+import './App.css';
+
+function App() {
+  const [initialData, setInitialData] = useState([{}])
+
+  useEffect(() => {
+    fetch('/api').then(
+      response => response.json()
+    ).then(data => setInitialData(data))
+  });
+  return (
+    <div className="App">
+      <h1>{initialData.title}</h1>
+      <br/>
+      <h2>{initialData.message}</h2>
+    </div>
+  );
+}
+
+export default App;
