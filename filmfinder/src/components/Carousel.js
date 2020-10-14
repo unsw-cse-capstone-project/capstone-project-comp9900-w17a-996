1ec221b12c51e9dc3bde306db881b3e1338804e6
JIAQI MA <jiaqima@JIAQIdeMacBook-Pro.local>
2020-10-14 13:51:57 +0800

    update

--- filmfinder/src/components/Carousel.js
+++ filmfinder/src/components/Carousel.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import "../styles/carousel.css";
-import poster from '../poster.jpg';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import poster from '../My perple, My Homeland(2020).jpg';
 import recom1 from '../rec1.jpg';
 import recom2 from '../rec2.jpg';
 import recom3 from '../rec3.jpg';
