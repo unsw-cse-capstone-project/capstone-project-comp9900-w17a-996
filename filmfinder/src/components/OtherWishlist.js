7f3c4f5961769bc3747c699a48209ca1a5243c7a
chenquzhao <chec98qu@gmail.com>
2020-10-31 22:11:25 +0800

    upload otherreview FE

---
+++ filmfinder/src/components/OtherWishlist.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { List } from 'antd';
+
+class OtherWishlist extends Component {
+    state = { titles: [
+        {
+            title: 'Ant Design Title 1',
+          },
+          {
+            title: 'Ant Design Title 2',
+          },
+          {
+            title: 'Ant Design Title 3',
+          },
+          {
+            title: 'Ant Design Title 4',
+          },
+    ] }
+
+
+    render() { 
+        return ( <React.Fragment>
+            <List
+    itemLayout="horizontal"
+    dataSource={this.state.titles}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          title={<a href="https://ant.design">{item.title}</a>}
+          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+        />
+      </List.Item>
+    )}
+  />
+        </React.Fragment> );
+    }
+}
+ 
+export default OtherWishlist;
